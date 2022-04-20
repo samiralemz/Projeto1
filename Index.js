8117fea636cd434db5b48e24a8e22b92f2c5bbf9
samiralemz <99972510+samiralemz@users.noreply.github.com>
2022-04-19 21:40:50 -0300

    Samira Up

---
+++ Index.js
@@ -0,0 +1,39 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+
+//criando a rota inicial
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo ao meu site novamente!</h1>");
+})
+
+//rota do cadastro de produtos
+app.get("/produtos", function(req,res){
+    res.send("<h1>Lista de Produtos!</h1>");
+})
+
+//rota com parametro 
+app.get("/consulta/:parametro", function(req,res){
+    //req --> dados enviados pelo cliente
+    //res --> resposta enviada pelo servidor de volta ao cliente
+    res.send("retorno consulta:" + req.params.parametro);
+})
+
+//rota com parametro opcional
+app.get("/cadastro/:nome?", function(req,res){
+    //req --> dados enviados pelo cliente
+    var nome = req.params.nome;
+    if (nome){
+        res.send("<h1>produto " + nome + " criado!</h1>");
+    }else{
+        res.send("produto criado!");
+    }
+    
+})
+
+app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 4000
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado.");
+    }
+})
